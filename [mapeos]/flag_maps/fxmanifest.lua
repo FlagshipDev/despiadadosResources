b2a3dc2bad832542e68eb0efe1e2cb04dc9ce38b
FlagshipDev <68760931+FlagshipDev@users.noreply.github.com>
2021-11-01 17:51:12 +0100

    Mapeos sin carpeta y borrado cochesVIP

---
+++ [mapeos]/flag_maps/fxmanifest.lua
@@ -0,0 +1,31 @@
+fx_version 'bodacious'
+game 'gta5'
+this_is_a_map 'yes'
+
+files {
+	'downtown.ymt',
+    'pillbox_hill.ymt',
+    'stream/gigz_karting_v1.1/stadium.ymt',
+    'stream/mountzonah/gta_props_fivem/timecycle_mods_1.xml',
+    'stream/mountzonah/urgence_floor/gusepe_timecycle_mods_1.xml'
+}
+
+
+data_file 'FIVEM_LOVES_YOU_A9E8B9F6EEB35186' 'stream/gigz_karting_v1/stadium.ymt'
+data_file 'SCENARIO_INFO_FILE' 'downtown.ymt'
+data_file 'SCENARIO_POINTS_PSO_FILE' 'downtown.ymt'
+data_file 'FIVEM_LOVES_YOU_A01627E3E5FC490A' 'downtown.ymt'
+data_file 'FIVEM_LOVES_YOU_399915A5EEB45A2B' 'downtown.ymt'
+data_file 'FIVEM_LOVES_YOU_A9E8B9F6EEB35186' 'downtown.ymt'
+data_file 'FIVEM_LOVES_YOU_DB4D236636684A3F' 'downtown.ymt'
+data_file 'SCENARIO_INFO_FILE' 'pillbox_hill.ymt'
+data_file 'SCENARIO_POINTS_PSO_FILE' 'pillbox_hill.ymt'
+data_file 'FIVEM_LOVES_YOU_A01627E3E5FC490A' 'pillbox_hill.ymt'
+data_file 'FIVEM_LOVES_YOU_399915A5EEB45A2B' 'pillbox_hill.ymt'
+data_file 'FIVEM_LOVES_YOU_A9E8B9F6EEB35186' 'pillbox_hill.ymt'
+data_file 'FIVEM_LOVES_YOU_DB4D236636684A3F' 'pillbox_hill.ymt'
+
+
+data_file 'DLC_ITYP_REQUEST' 'stream/mountzonah/gta_props_fivem/vw_prop_vw_casino_art_02.ytyp'
+data_file 'TIMECYCLEMOD_FILE' 'stream/mountzonah/gta_props_fivem/timecycle_mods_1.xml'
+data_file 'TIMECYCLEMOD_FILE' 'stream/mountzonah/urgence_floor/gusepe_timecycle_mods_1.xml'
