d31d20923b8b94c54b4b2ebfb29995baae45b9f0
FlagshipDev <68760931+FlagshipDev@users.noreply.github.com>
2021-11-01 18:08:03 +0100

    Último mapeo [coches]

---
+++ [mapeos]/[Vehicles]/flag_coches/fxmanifest.lua
@@ -0,0 +1,523 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'ESX Personal Menu'
+
+Author 'Flagship - Dev For LeyendasRP'
+
+version '1.0.0'
+
+data_file('DLC_ITYP_REQUEST')('stream/escudo/shield.ytyp')
+
+files {
+
+    --camaro2021
+    'meta/camaro2021/handling.meta',
+    'meta/camaro2021/vehicles.meta',
+    'meta/camaro2021/carcols.meta',
+    'meta/camaro2021/carvariations.meta',
+	'meta/camaro2021/dlctext.meta',
+	
+	--code311cvpi
+	'meta/code311cvpi/handling.meta',
+	'meta/code311cvpi/vehicles.meta',
+	'meta/code311cvpi/carcols.meta',
+	'meta/code311cvpi/carvariations.meta',
+	
+	--code314charg
+	'meta/code314charg/handling.meta',
+	'meta/code314charg/vehicles.meta',
+	'meta/code314charg/carcols.meta',
+	'meta/code314charg/carvariations.meta',
+	
+	--code316fpiu
+	'meta/code316fpiu/handling.meta',
+	'meta/code316fpiu/vehicles.meta',
+	'meta/code316fpiu/carcols.meta',
+	'meta/code316fpiu/carvariations.meta',
+	
+	--code316impala
+	'meta/code316impala/handling.meta',
+	'meta/code316impala/vehicles.meta',
+	'meta/code316impala/carcols.meta',
+	'meta/code316impala/carvariations.meta',
+	
+	--code318charg
+	'meta/code318charg/handling.meta',
+	'meta/code318charg/vehicles.meta',
+	'meta/code318charg/carcols.meta',
+	'meta/code318charg/carvariations.meta',
+	
+	--code318tahoe
+	'meta/code318tahoe/handling.meta',
+	'meta/code318tahoe/vehicles.meta',
+	'meta/code318tahoe/carcols.meta',
+	'meta/code318tahoe/carvariations.meta',
+	
+	--code320exp
+	'meta/code320exp/handling.meta',
+	'meta/code320exp/vehicles.meta',
+	'meta/code320exp/carcols.meta',
+	'meta/code320exp/carvariations.meta',
+	
+	--code3bmw
+	'meta/code3bmw/handling.meta',
+	'meta/code3bmw/vehicles.meta',
+	'meta/code3bmw/carcols.meta',
+	'meta/code3bmw/carvariations.meta',
+	
+	--code3camero
+--	'meta/code3camero/handling.meta',
+--	'meta/code3camero/vehicles.meta',
+--	'meta/code3camero/carcols.meta',
+--	'meta/code3camero/carvariations.meta',
+	
+	--code3dirtbike
+	'meta/code3dirtbike/handling.meta',
+	'meta/code3dirtbike/vehicles.meta',
+	'meta/code3dirtbike/carcols.meta',
+	'meta/code3dirtbike/carvariations.meta',
+
+	--hondacbr
+	'meta/hondacbr/handling.meta',
+	'meta/hondacbr/vehicles.meta',
+	'meta/hondacbr/carcols.meta',
+	'meta/hondacbr/carvariations.meta',
+	'meta/hondacbr/dlctext.meta',
+
+	--bmwbs17
+	'meta/bmwbs17/handling.meta',
+	'meta/bmwbs17/vehicles.meta',
+	'meta/bmwbs17/carcols.meta',
+	'meta/bmwbs17/carvariations.meta',
+	'meta/bmwbs17/dlctext.meta',
+	
+	--code3fpis
+	'meta/code3fpis/handling.meta',
+	'meta/code3fpis/vehicles.meta',
+	'meta/code3fpis/carcols.meta',
+	'meta/code3fpis/carvariations.meta',
+	
+	--code3mustang
+	'meta/code3mustang/handling.meta',
+	'meta/code3mustang/vehicles.meta',
+	'meta/code3mustang/carcols.meta',
+	'meta/code3mustang/carvariations.meta',
+	
+	--code3ram
+	'meta/code3ram/handling.meta',
+	'meta/code3ram/vehicles.meta',
+	'meta/code3ram/carcols.meta',
+	'meta/code3ram/carvariations.meta',
+
+	--policet
+	'meta/policet/carcols.meta',
+	'meta/policet/carvariations.meta',
+	'meta/policet/dlctext.meta',
+	
+	--code3tahoek9
+--	'meta/code3tahoek9/handling.meta',
+--	'meta/code3tahoek9/vehicles.meta',
+--	'meta/code3tahoek9/carcols.meta',
+--	'meta/code3tahoek9/carvariations.meta',
+	
+	--code398cvpi
+--	'meta/code398cvpi/handling.meta',
+--	'meta/code398cvpi/vehicles.meta',
+--	'meta/code398cvpi/carcols.meta',
+--	'meta/code398cvpi/carvariations.meta',
+	
+	--pbike
+	'meta/pbike/vehicles.meta',
+	'meta/pbike/carcols.meta',
+	'meta/pbike/carvariations.meta',
+	
+	--flatbed3
+	'meta/flatbed3/vehicles.meta',
+	'meta/flatbed3/handling.meta',
+	'meta/flatbed3/carcols.meta',
+	'meta/flatbed3/carvariations.meta',
+
+	--LSFD
+	'meta/lsfd/vehicles.meta',
+	'meta/lsfd/handling.meta',
+	'meta/lsfd/carcols.meta',
+	'meta/lsfd/carvariations.meta',
+	'meta/lsfd/dlctext.meta',
+
+	--Helicoptero SAPD
+	'meta/helisapd/vehicles.meta',
+	'meta/helisapd/handling.meta',
+	'meta/helisapd/dlctext.meta',
+
+	--Undercover
+	'meta/undercover/vehicles.meta',
+	'meta/undercover/handling.meta',
+	'meta/undercover/carcols.meta',
+	'meta/undercover/carvariations.meta',
+
+	--taxi
+	'meta/taxi/vehicles.meta',
+	'meta/taxi/handling.meta',
+	'meta/taxi/carcols.meta',
+	'meta/taxi/carvariations.meta',
+
+	--swatsign 
+	'meta/swatsign/vehicles.meta',
+	'meta/swatsign/handling.meta',
+	'meta/swatsign/carcols.meta',
+	'meta/swatsign/carvariations.meta',
+
+	--Tabla de surf
+	'meta/surf/vehicles.meta',
+    'meta/surf/carvariations.meta',
+    'meta/surf/carcols.meta',
+    'meta/surf/handling.meta',    
+	'meta/surf/dlctext.meta',
+	
+	--bx2018
+	'meta/bx2018/vehicles.meta',
+	'meta/bx2018/handling.meta',
+	'meta/bx2018/carcols.meta',
+	'meta/bx2018/carvariations.meta',
+	'meta/bx2018/dlctext.meta',
+
+	--santafe
+	'meta/santafe/vehicles.meta',
+	'meta/santafe/handling.meta',
+	'meta/santafe/carcols.meta',
+	'meta/santafe/carvariations.meta',
+	'meta/santafe/dlctext.meta',
+
+	--19raptor
+	'meta/19raptor/vehicles.meta',
+	'meta/19raptor/handling.meta',
+	'meta/19raptor/carcols.meta',
+	'meta/19raptor/carvariations.meta',
+	'meta/19raptor/dlctext.meta',
+
+	--r820
+	'meta/r820/vehicles.meta',
+	'meta/r820/handling.meta',
+	'meta/r820/carcols.meta',
+	'meta/r820/carvariations.meta',
+
+	--488gtb
+	'meta/488gtb/vehicles.meta',
+	'meta/488gtb/handling.meta',
+	'meta/488gtb/carcols.meta',
+	'meta/488gtb/carvariations.meta',
+
+	--panigale
+	'meta/panigale/vehicles.meta',
+	'meta/panigale/handling.meta',
+	'meta/panigale/carcols.meta',
+	'meta/panigale/carvariations.meta',
+
+	--zx10
+	'meta/zx10/vehicles.meta',
+	'meta/zx10/handling.meta',
+	'meta/zx10/carcols.meta',
+	'meta/zx10/carvariations.meta',
+
+	--yzfr6
+	'meta/yzfr6/vehicles.meta',
+	'meta/yzfr6/handling.meta',
+	'meta/yzfr6/carcols.meta',
+	'meta/yzfr6/carvariations.meta',
+
+	--camaropol
+	'meta/camaropol/vehicles.meta',
+	'meta/camaropol/carcols.meta',
+	'meta/camaropol/carvariations.meta',
+
+	--19zr1
+	'meta/19zr1/vehicles.meta',
+	'meta/19zr1/handling.meta',
+	'meta/19zr1/carcols.meta',
+	'meta/19zr1/carvariations.meta',
+
+	--18performante
+	'meta/18performante/vehicles.meta',
+	'meta/18performante/handling.meta',
+	'meta/18performante/carvariations.meta',
+	'meta/18performante/carcols.meta',
+
+	--corvettepol
+	'meta/corvettepol/vehicles.meta',
+	'meta/corvettepol/handling.meta',
+	'meta/corvettepol/carvariations.meta',
+	'meta/corvettepol/carcols.meta',
+
+	--corvette8
+	'meta/corvette8/vehicles.meta',
+	'meta/corvette8/handling.meta',
+    'meta/corvette8/carvariations.meta',
+
+	--golf75r
+	'meta/golf75r/vehicles.meta',
+	'meta/golf75r/handling.meta',
+	'meta/golf75r/carvariations.meta',
+	'meta/golf75r/carcols.meta',
+
+	--challengerd
+	'meta/challengerd/vehicles.meta',
+	'meta/challengerd/handling.meta',
+	'meta/challengerd/carvariations.meta',
+	'meta/challengerd/carcols.meta',
+
+	}
+
+	--camaro2021
+	data_file 'HANDLING_FILE' 'meta/camaro2021/handling.meta'
+	data_file 'VEHICLE_METADATA_FILE' 'meta/camaro2021/vehicles.meta'
+	data_file 'CARCOLS_FILE' 'meta/camaro2021/carcols.meta'
+	data_file 'VEHICLE_VARIATION_FILE' 'meta/camaro2021/carvariations.meta'
+	
+	--code311cvpi
+	data_file 'HANDLING_FILE' 'meta/code311cvpi/handling.meta'
+	data_file 'VEHICLE_METADATA_FILE' 'meta/code311cvpi/vehicles.meta'
+	data_file 'CARCOLS_FILE' 'meta/code311cvpi/carcols.meta'
+	data_file 'VEHICLE_VARIATION_FILE' 'meta/code311cvpi/carvariations.meta'
+	
+	--code314charg
+	data_file 'HANDLING_FILE' 'meta/code314charg/handling.meta'
+	data_file 'VEHICLE_METADATA_FILE' 'meta/code314charg/vehicles.meta'
+	data_file 'CARCOLS_FILE' 'meta/code314charg/carcols.meta'
+	data_file 'VEHICLE_VARIATION_FILE' 'meta/code314charg/carvariations.meta'
+	
+	--code316fpiu
+	data_file 'HANDLING_FILE' 'meta/code316fpiu/handling.meta'
+	data_file 'VEHICLE_METADATA_FILE' 'meta/code316fpiu/vehicles.meta'
+	data_file 'CARCOLS_FILE' 'meta/code316fpiu/carcols.meta'
+	data_file 'VEHICLE_VARIATION_FILE' 'meta/code316fpiu/carvariations.meta'
+	
+	--code316impala
+	data_file 'HANDLING_FILE' 'meta/code316impala/handling.meta'
+	data_file 'VEHICLE_METADATA_FILE' 'meta/code316impala/vehicles.meta'
+	data_file 'CARCOLS_FILE' 'meta/code316impala/carcols.meta'
+	data_file 'VEHICLE_VARIATION_FILE' 'meta/code316impala/carvariations.meta'
+	
+	--code318charg
+	data_file 'HANDLING_FILE' 'meta/code318charg/handling.meta'
+	data_file 'VEHICLE_METADATA_FILE' 'meta/code318charg/vehicles.meta'
+	data_file 'CARCOLS_FILE' 'meta/code318charg/carcols.meta'
+	data_file 'VEHICLE_VARIATION_FILE' 'meta/code318charg/carvariations.meta'
+	
+	--code318tahoe
+	data_file 'HANDLING_FILE' 'meta/code318tahoe/handling.meta'
+	data_file 'VEHICLE_METADATA_FILE' 'meta/code318tahoe/vehicles.meta'
+	data_file 'CARCOLS_FILE' 'meta/code318tahoe/carcols.meta'
+	data_file 'VEHICLE_VARIATION_FILE' 'meta/code318tahoe/carvariations.meta'
+	
+	--code320exp
+	data_file 'HANDLING_FILE' 'meta/code320exp/handling.meta'
+	data_file 'VEHICLE_METADATA_FILE' 'meta/code320exp/vehicles.meta'
+	data_file 'CARCOLS_FILE' 'meta/code320exp/carcols.meta'
+	data_file 'VEHICLE_VARIATION_FILE' 'meta/code320exp/carvariations.meta'
+	
+	--code3bmw
+	data_file 'HANDLING_FILE' 'meta/code3bmw/handling.meta'
+	data_file 'VEHICLE_METADATA_FILE' 'meta/code3bmw/vehicles.meta'
+	data_file 'CARCOLS_FILE' 'meta/code3bmw/carcols.meta'
+	data_file 'VEHICLE_VARIATION_FILE' 'meta/code3bmw/carvariations.meta'
+	
+	--code3camero
+	data_file 'HANDLING_FILE' 'meta/code3camero/handling.meta'
+	data_file 'VEHICLE_METADATA_FILE' 'meta/code3camero/vehicles.meta'
+	data_file 'CARCOLS_FILE' 'meta/code3camero/carcols.meta'
+	data_file 'VEHICLE_VARIATION_FILE' 'meta/code3camero/carvariations.meta'
+	
+	--code3dirtbike
+	data_file 'HANDLING_FILE' 'meta/code3dirtbike/handling.meta'
+	data_file 'VEHICLE_METADATA_FILE' 'meta/code3dirtbike/vehicles.meta'
+	data_file 'CARCOLS_FILE' 'meta/code3dirtbike/carcols.meta'
+	data_file 'VEHICLE_VARIATION_FILE' 'meta/code3dirtbike/carvariations.meta'
+	
+	--code3fpis
+	data_file 'HANDLING_FILE' 'meta/code3fpis/handling.meta'
+	data_file 'VEHICLE_METADATA_FILE' 'meta/code3fpis/vehicles.meta'
+	data_file 'CARCOLS_FILE' 'meta/code3fpis/carcols.meta'
+	data_file 'VEHICLE_VARIATION_FILE' 'meta/code3fpis/carvariations.meta'
+	
+	--code3mustang
+	data_file 'HANDLING_FILE' 'meta/code3mustang/handling.meta'
+	data_file 'VEHICLE_METADATA_FILE' 'meta/code3mustang/vehicles.meta'
+	data_file 'CARCOLS_FILE' 'meta/code3mustang/carcols.meta'
+	data_file 'VEHICLE_VARIATION_FILE' 'meta/code3mustang/carvariations.meta'
+	
+	--code3ram
+	data_file 'HANDLING_FILE' 'meta/code3ram/handling.meta'
+	data_file 'VEHICLE_METADATA_FILE' 'meta/code3ram/vehicles.meta'
+	data_file 'CARCOLS_FILE' 'meta/code3ram/carcols.meta'
+	data_file 'VEHICLE_VARIATION_FILE' 'meta/code3ram/carvariations.meta'
+	
+	--code3tahoek9
+	data_file 'HANDLING_FILE' 'meta/code3tahoek9/handling.meta'
+	data_file 'VEHICLE_METADATA_FILE' 'meta/code3tahoek9/vehicles.meta'
+	data_file 'CARCOLS_FILE' 'meta/code3tahoek9/carcols.meta'
+	data_file 'VEHICLE_VARIATION_FILE' 'meta/code3tahoek9/carvariations.meta'
+
+	--pbike
+	data_file 'VEHICLE_METADATA_FILE' 'meta/pbike/vehicles.meta'
+	data_file 'CARCOLS_FILE' 'meta/pbike/carcols.meta'
+	data_file 'VEHICLE_VARIATION_FILE' 'meta/pbike/carvariations.meta'
+	
+	--code398cvpi
+	data_file 'HANDLING_FILE' 'meta/code398cvpi/handling.meta'
+	data_file 'VEHICLE_METADATA_FILE' 'meta/code398cvpi/vehicles.meta'
+	data_file 'CARCOLS_FILE' 'meta/code398cvpi/carcols.meta'
+	data_file 'VEHICLE_VARIATION_FILE' 'meta/code398cvpi/carvariations.meta'
+
+	--flatbed3
+	data_file 'HANDLING_FILE' 'meta/flatbed3/handling.meta'
+	data_file 'VEHICLE_METADATA_FILE' 'meta/flatbed3/vehicles.meta'
+	data_file 'CARCOLS_FILE' 'meta/flatbed3/carcols.meta'
+	data_file 'VEHICLE_VARIATION_FILE' 'meta/flatbed3/carvariations.meta'
+
+	--LSFD
+	data_file 'HANDLING_FILE' 'meta/lsfd/handling.meta'
+	data_file 'VEHICLE_METADATA_FILE' 'meta/lsfd/vehicles.meta'
+	data_file 'CARCOLS_FILE' 'meta/lsfd/carcols.meta'
+	data_file 'VEHICLE_VARIATION_FILE' 'meta/lsfd/carvariations.meta'
+	data_file 'DLCTEXT_FILE' 'meta/lsfd/dlctext.meta'
+
+	--Helicoptero SAPD
+	data_file 'HANDLING_FILE' 'meta/helisapd/handling.meta'
+	data_file 'VEHICLE_METADATA_FILE' 'meta/helisapd/vehicles.meta'
+	data_file 'DLCTEXT_FILE' 'meta/helisapd/dlctext.meta'
+
+	--Undercover
+	data_file 'HANDLING_FILE' 'meta/undercover/handling.meta'
+	data_file 'VEHICLE_METADATA_FILE' 'meta/undercover/vehicles.meta'
+	data_file 'CARCOLS_FILE' 'meta/undercover/carcols.meta'
+	data_file 'VEHICLE_VARIATION_FILE' 'meta/undercover/carvariations.meta'
+
+	--Taxi
+	data_file 'HANDLING_FILE' 'meta/taxi/handling.meta'
+	data_file 'VEHICLE_METADATA_FILE' 'meta/taxi/vehicles.meta'
+	data_file 'CARCOLS_FILE' 'meta/taxi/carcols.meta'
+	data_file 'VEHICLE_VARIATION_FILE' 'meta/taxi/carvariations.meta'
+
+	--Taxi
+	data_file 'HANDLING_FILE' 'meta/swatsign/handling.meta'
+	data_file 'VEHICLE_METADATA_FILE' 'meta/swatsign/vehicles.meta'
+	data_file 'CARCOLS_FILE' 'meta/swatsign/carcols.meta'
+	data_file 'VEHICLE_VARIATION_FILE' 'meta/swatsign/carvariations.meta'
+
+    --surf
+	data_file 'HANDLING_FILE' 'meta/surf/handling.meta'
+	data_file 'VEHICLE_METADATA_FILE' 'meta/surf/vehicles.meta'
+	data_file 'CARCOLS_FILE' 'meta/surf/carcols.meta'
+	data_file 'VEHICLE_VARIATION_FILE' 'meta/surf/carvariations.meta'
+	data_file 'DLC_TEXT_FILE' 'meta/surf/dlctext.meta'
+
+	--hondacbr
+	data_file 'HANDLING_FILE' 'meta/hondacbr/handling.meta'
+	data_file 'VEHICLE_METADATA_FILE' 'meta/hondacbr/vehicles.meta'
+	data_file 'CARCOLS_FILE' 'meta/hondacbr/carcols.meta'
+	data_file 'VEHICLE_VARIATION_FILE' 'meta/hondacbr/carvariations.meta'
+	data_file 'DLCTEXT_FILE' 'meta/hondacbr/dlctext.meta'
+
+	--bmwbs17
+	data_file 'HANDLING_FILE' 'meta/bmwbs17/handling.meta'
+	data_file 'VEHICLE_METADATA_FILE' 'meta/bmwbs17/vehicles.meta'
+	data_file 'CARCOLS_FILE' 'meta/bmwbs17/carcols.meta'
+	data_file 'VEHICLE_VARIATION_FILE' 'meta/bmwbs17/carvariations.meta'
+	data_file 'DLCTEXT_FILE' 'meta/bmwbs17/dlctext.meta'
+
+	--bx2018
+	data_file 'HANDLING_FILE' 'meta/bx2018/handling.meta'
+	data_file 'VEHICLE_METADATA_FILE' 'meta/bx2018/vehicles.meta'
+	data_file 'CARCOLS_FILE' 'meta/bx2018/carcols.meta'
+	data_file 'VEHICLE_VARIATION_FILE' 'meta/bx2018/carvariations.meta'
+	data_file 'DLCTEXT_FILE' 'meta/bx2018/dlctext.meta'
+
+	--santafe
+	data_file 'HANDLING_FILE' 'meta/santafe/handling.meta'
+	data_file 'VEHICLE_METADATA_FILE' 'meta/santafe/vehicles.meta'
+	data_file 'CARCOLS_FILE' 'meta/santafe/carcols.meta'
+	data_file 'VEHICLE_VARIATION_FILE' 'meta/santafe/carvariations.meta'
+	data_file 'DLCTEXT_FILE' 'meta/santafe/dlctext.meta'
+
+	--19raptor
+	data_file 'HANDLING_FILE' 'meta/19raptor/handling.meta'
+	data_file 'VEHICLE_METADATA_FILE' 'meta/19raptor/vehicles.meta'
+	data_file 'CARCOLS_FILE' 'meta/19raptor/carcols.meta'
+	data_file 'VEHICLE_VARIATION_FILE' 'meta/19raptor/carvariations.meta'
+
+	--policet
+	data_file 'CARCOLS_FILE' 'meta/policet/carcols.meta'
+	data_file 'VEHICLE_VARIATION_FILE' 'meta/policet/carvariations.meta'
+	data_file 'DLCTEXT_FILE' 'meta/policet/dlctext.meta'
+
+	--r820
+	data_file 'HANDLING_FILE' 'meta/r820/handling.meta'
+	data_file 'VEHICLE_METADATA_FILE' 'meta/r820/vehicles.meta'
+	data_file 'CARCOLS_FILE' 'meta/r820/carcols.meta'
+	data_file 'VEHICLE_VARIATION_FILE' 'meta/r820/carvariations.meta'
+
+	--488gtb
+	data_file 'HANDLING_FILE' 'meta/488gtb/handling.meta'
+	data_file 'VEHICLE_METADATA_FILE' 'meta/488gtb/vehicles.meta'
+	data_file 'CARCOLS_FILE' 'meta/488gtb/carcols.meta'
+	data_file 'VEHICLE_VARIATION_FILE' 'meta/488gtb/carvariations.meta'
+
+	--panigale
+	data_file 'HANDLING_FILE' 'meta/panigale/handling.meta'
+	data_file 'VEHICLE_METADATA_FILE' 'meta/panigale/vehicles.meta'
+	data_file 'CARCOLS_FILE' 'meta/panigale/carcols.meta'
+	data_file 'VEHICLE_VARIATION_FILE' 'meta/panigale/carvariations.meta'
+
+	--zx10
+	data_file 'HANDLING_FILE' 'meta/zx10/handling.meta'
+	data_file 'VEHICLE_METADATA_FILE' 'meta/zx10/vehicles.meta'
+	data_file 'CARCOLS_FILE' 'meta/zx10/carcols.meta'
+	data_file 'VEHICLE_VARIATION_FILE' 'meta/zx10/carvariations.meta'
+
+	--yzfr6
+	data_file 'HANDLING_FILE' 'meta/yzfr6/handling.meta'
+	data_file 'VEHICLE_METADATA_FILE' 'meta/yzfr6/vehicles.meta'
+	data_file 'CARCOLS_FILE' 'meta/yzfr6/carcols.meta'
+	data_file 'VEHICLE_VARIATION_FILE' 'meta/yzfr6/carvariations.meta'
+
+	--camaropol
+	data_file 'VEHICLE_METADATA_FILE' 'meta/camaropol/vehicles.meta'
+	data_file 'CARCOLS_FILE' 'meta/camaropol/carcols.meta'
+	data_file 'VEHICLE_VARIATION_FILE' 'meta/camaropol/carvariations.meta'
+
+	--19zr1
+	data_file 'HANDLING_FILE' 'meta/19zr1/handling.meta'
+	data_file 'VEHICLE_METADATA_FILE' 'meta/19zr1/vehicles.meta'
+	data_file 'CARCOLS_FILE' 'meta/19zr1/carcols.meta'
+	data_file 'VEHICLE_VARIATION_FILE' 'meta/19zr1/carvariations.meta'
+
+	--18performante
+	data_file 'HANDLING_FILE' 'meta/18performante/handling.meta'
+	data_file 'VEHICLE_METADATA_FILE' 'meta/18performante/vehicles.meta'
+	data_file 'CARCOLS_FILE' 'meta/18performante/carcols.meta'
+	data_file 'VEHICLE_VARIATION_FILE' 'meta/18performante/carvariations.meta'
+
+	--corvettepol
+	data_file 'HANDLING_FILE' 'meta/corvettepol/handling.meta'
+	data_file 'VEHICLE_METADATA_FILE' 'meta/corvettepol/vehicles.meta'
+	data_file 'CARCOLS_FILE' 'meta/corvettepol/carcols.meta'
+	data_file 'VEHICLE_VARIATION_FILE' 'meta/corvettepol/carvariations.meta'
+
+	--corvette8
+	data_file 'HANDLING_FILE' 'meta/corvette8/handling.meta'
+	data_file 'VEHICLE_METADATA_FILE' 'meta/corvette8/vehicles.meta'
+	data_file 'VEHICLE_VARIATION_FILE' 'meta/corvette8/carvariations.meta'
+
+	--golf75r
+	data_file 'HANDLING_FILE' 'meta/golf75r/handling.meta'
+	data_file 'VEHICLE_METADATA_FILE' 'meta/golf75r/vehicles.meta'
+	data_file 'CARCOLS_FILE' 'meta/golf75r/carcols.meta'
+	data_file 'VEHICLE_VARIATION_FILE' 'meta/golf75r/carvariations.meta'
+
+	--challengerd
+	data_file 'HANDLING_FILE' 'meta/challengerd/handling.meta'
+	data_file 'VEHICLE_METADATA_FILE' 'meta/challengerd/vehicles.meta'
+	data_file 'CARCOLS_FILE' 'meta/challengerd/carcols.meta'
+	data_file 'VEHICLE_VARIATION_FILE' 'meta/challengerd/carvariations.meta'
