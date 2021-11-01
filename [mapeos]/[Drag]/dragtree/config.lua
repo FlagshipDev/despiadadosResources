c49d4a8ca15e459b3dd35620b2b9544a02f19134
FlagshipDev <68760931+FlagshipDev@users.noreply.github.com>
2021-11-01 17:46:32 +0100

    Mapeo [Drag]

---
+++ [mapeos]/[Drag]/dragtree/config.lua
@@ -0,0 +1,22 @@
+Config = {}
+
+Config.position = {1693.54, 3247.91, 41.21, 285.00}
+
+-- yellow (1-6)
+-- green (7, 8)
+-- red (9, 10)
+-- nothing {}
+
+Config.program = {
+    {1},{1,2},
+
+    {1,2,3},{1,2,3,4},
+
+    {}, -- disable all lights
+
+    {1,2}, 
+    {3,4}, 
+    {5,6}, 
+
+    {7,8}
+}
