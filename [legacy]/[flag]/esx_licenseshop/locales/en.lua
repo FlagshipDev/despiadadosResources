55ff1d90235f32741b9f7426cab92c7e86a31340
FlagshipDev <68760931+FlagshipDev@users.noreply.github.com>
2021-11-04 22:59:16 +0100

    Update LeyendasRP resources (flag_utils)

---
+++ [legacy]/[flag]/esx_licenseshop/locales/en.lua
@@ -0,0 +1,28 @@
+Locales['en'] = {
+  ['buy_license'] = '¿Que licencia quieres comprar?',
+  ['press_access'] = 'Presiona     ~INPUT_CONTEXT~ para acceder a la tienda',
+  ['not_enough_money'] = 'No tienes dinero suficiente',
+  ['blip_license_shop'] = 'Licencias | Tienda',
+  ['need_dmv'] = 'You must have a Drivers Permit',
+  ['shop_menu_item'] = '$%s',
+  -- esx_advancedvehicleshop Licenses
+  ['license_aircraft'] = 'Licencia de vuelo',
+  ['license_boating'] = 'Licencia de navegación',
+  -- esx_advancedweaponshop Licenses
+  ['license_melee'] = 'Melee License',
+  ['license_handgun'] = 'Handgun License',
+  ['license_smg'] = 'SMG License',
+  ['license_shotgun'] = 'Shotgun License',
+  ['license_assault'] = 'Assault Rifle License',
+  ['license_lmg'] = 'LMG License',
+  ['license_sniper'] = 'Sniper Rifle License',
+  -- esx_dmvschool Licenses
+  ['license_commercial'] = 'Licencia de vehículos pesados',
+  ['license_drivers'] = 'Licencia de turismos',
+  ['license_driversp'] = 'Drivers Permit',
+  ['license_motorcycle'] = 'Licencia de motocicletas',
+  -- esx_drugs Licenses
+  ['license_weed'] = 'Weed License',
+  -- esx_weashop Licenses
+  ['license_weapon'] = 'Licencia de armas',
+}
---
+++ [legacy]/[flag]/flag_utils/config.lua
@@ -0,0 +1,28 @@
+Config = {}
+Config.ClientID =  771833265560485900 -- Put your discord bot client id here
+Config.PlayerCount = 500
+Config.PlayerText = "Jugadores" -- Player text. Example Players 10/32
+Config.ResourceTimer = 10 -- Time in witch resource updates in seconds
+Config.UseESXIdentity = true -- Uses ESX Identity name not steam name
+
+
+cfg = {}
+--This is where you put your DiscordAppID you can get it from here https://discord.com/developers/applications
+cfg.DiscordAppID = 771833265560485900 
+
+cfg.discordImageName = 'gltich'
+cfg.hoverText = 'Leyendas RP v2'
+cfg.richPresenceText = 'Jugando a Leyendas RP'
+cfg.smallDiscordImageName = 'logo'
+cfg.smallHoverText = 'discord.gg/wwbxcC8Qp4'
+
+cfg.button1 = {
+    text = 'Discord',
+    url = 'discord.gg/wwbxcC8Qp4'
+}
+
+cfg.button2 = {
+    text = 'Web',
+    url = 'https://leyendasrp.es'
+}
+-- Just to clarify something, you can only have 2 buttons
